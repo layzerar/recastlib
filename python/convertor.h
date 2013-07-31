f066e3f119afeb6e616ac33dc8a7617e2680c7d7
layz <layzerar@gmail.com>
2013-08-01 00:58:07 +0800

    Fix reference leak of container convertor.

--- python/convertor.h
+++ python/convertor.h
@@ -18,8 +18,12 @@ namespace converter = boost::python::converter;
 template<class T>
 struct list_to_list_converter {
 	static PyObject* convert(const std::list<T>& v) {
-		using namespace boost::python;
-		list pylist; FOREACH(const T& e, v) pylist.append(e);
+		using boost::python::list;
+		using boost::python::incref;
+
+		list pylist;
+		FOREACH(const T& e, v)
+			pylist.append(e);
 		return incref(pylist.ptr());
 	}
 };
@@ -27,8 +31,12 @@ struct list_to_list_converter {
 template<class T>
 struct vector_to_list_converter {
 	static PyObject* convert(const std::vector<T>& v) {
-		using namespace boost::python;
-		list pylist; FOREACH(const T& e, v) pylist.append(e);
+		using boost::python::list;
+		using boost::python::incref;
+
+		list pylist;
+		FOREACH(const T& e, v)
+			pylist.append(e);
 		return incref(pylist.ptr());
 	}
 };
@@ -36,7 +44,9 @@ struct vector_to_list_converter {
 template<class T1, class T2>
 struct pair_to_tuple_converter {
 	static PyObject* convert(const std::pair<T1, T2>& pair) {
-		using namespace boost::python;
+		using boost::python::incref;
+		using boost::python::make_tuple;
+
 		return incref(make_tuple(pair.first, pair.second).ptr());
 	}
 };
@@ -48,24 +58,27 @@ struct list_from_seq_converter {
 		converter::registry::push_back(&convertible, &construct, type_id<std::list<T> >());
 	}
 
-	static void* convertible(PyObject* obj_ptr){
+	static void* convertible(PyObject* obj_ptr) {
 		// the second condition is important, for some reason otherwise there were attempted conversions of Body to list which failed afterwards.
 		if(!PySequence_Check(obj_ptr) || !PyObject_HasAttrString(obj_ptr, "__len__"))
 			return NULL;
 		return obj_ptr;
 	}
 
-	static void construct(PyObject* obj_ptr, converter::rvalue_from_python_stage1_data* data){
+	static void construct(PyObject* obj_ptr, converter::rvalue_from_python_stage1_data* data) {
+		using boost::python::handle;
 		using boost::python::extract;
+
 		void* storage = ((converter::rvalue_from_python_storage<std::list<T> >*)(data))->storage.bytes;
 		new (storage) std::list<T>();
 		std::vector<T>* v = (std::list<T>*)(storage);
-		int vlen = PySequence_Size(obj_ptr);
+		Py_ssize_t vlen = PySequence_Size(obj_ptr);
 		if(vlen < 0)
 			abort();
 		v->reserve(vlen);
 		for(int i = 0; i < vlen; i++) {
-			v->push_back(extract<T>(PySequence_GetItem(obj_ptr, i)));
+			handle<> item(PySequence_GetItem(obj_ptr, i));
+			v->push_back(extract<T>(item.get()));
 		}
 		data->convertible = storage;
 	}
@@ -78,24 +91,27 @@ struct vector_from_seq_converter {
 		converter::registry::push_back(&convertible, &construct, type_id<std::vector<T> >());
 	}
 
-	static void* convertible(PyObject* obj_ptr){
+	static void* convertible(PyObject* obj_ptr) {
 		// the second condition is important, for some reason otherwise there were attempted conversions of Body to list which failed afterwards.
 		if(!PySequence_Check(obj_ptr) || !PyObject_HasAttrString(obj_ptr, "__len__"))
 			return NULL;
 		return obj_ptr;
 	}
 
-	static void construct(PyObject* obj_ptr, converter::rvalue_from_python_stage1_data* data){
+	static void construct(PyObject* obj_ptr, converter::rvalue_from_python_stage1_data* data) {
+		using boost::python::handle;
 		using boost::python::extract;
+
 		void* storage = ((converter::rvalue_from_python_storage<std::vector<T> >*)(data))->storage.bytes;
 		new (storage) std::vector<T>();
 		std::vector<T>* v = (std::vector<T>*)(storage);
-		int vlen = PySequence_Size(obj_ptr);
+		Py_ssize_t vlen = PySequence_Size(obj_ptr);
 		if(vlen < 0)
 			abort();
 		v->reserve(vlen);
 		for(int i = 0; i < vlen; i++) {
-			v->push_back(extract<T>(PySequence_GetItem(obj_ptr, i)));
+			handle<> item(PySequence_GetItem(obj_ptr, i));
+			v->push_back(extract<T>(item.get()));
 		}
 		data->convertible = storage;
 	}
@@ -115,8 +131,8 @@ struct dtvec3_from_seq_convertor {
 		return obj_ptr;
 	}
 
-	static void construct(PyObject* obj_ptr,
-			converter::rvalue_from_python_stage1_data* data) {
+	static void construct(PyObject* obj_ptr, converter::rvalue_from_python_stage1_data* data) {
+		using boost::python::handle;
 		using boost::python::extract;
 		dtAssert(PySequence_Size(obj_ptr) == 3);
 
@@ -124,9 +140,12 @@ struct dtvec3_from_seq_convertor {
 		new (storage) dtVec3;
 		dtVec3* v3 = (dtVec3*) storage;
 
-		v3->x = extract<float>(PySequence_GetItem(obj_ptr, 0));
-		v3->y = extract<float>(PySequence_GetItem(obj_ptr, 1));
-		v3->z = extract<float>(PySequence_GetItem(obj_ptr, 2));
+		handle<> x(PySequence_GetItem(obj_ptr, 0));
+		handle<> y(PySequence_GetItem(obj_ptr, 1));
+		handle<> z(PySequence_GetItem(obj_ptr, 2));
+		v3->x = extract<float>(x.get());
+		v3->y = extract<float>(y.get());
+		v3->z = extract<float>(z.get());
 		data->convertible = storage;
 	}
 };
